ed6e85aeb6e259904cf88f45e96ca3a6275e9a75
MINGYEOL KIM <kmg1902@postech.ac.kr>
2022-12-10 00:19:59 +0900

    refactor : move validationWorkerOrdering to master-functions branch

--- src/main/scala/mergeDoneRelated/MergeDoneRelated.scala
+++ src/main/scala/mergeDoneRelated/MergeDoneRelated.scala
@@ -17,34 +17,4 @@ object MergeDoneRelated {
     val maxKey = extractMinMaxKeyAux(minKey)
     (minKey, maxKey)
   }
-
-  // Assume that always Pivot._1 <= Pivot._2
-  def validationWorkerOrdering(workerToMinMaxKey: Map[Worker, Pivot], workers: List[Worker]): Boolean = {
-    def validateMinMaxKeyDependency(prevMinMaxKeyOpt: Option[Pivot], currMinMaxKey: Pivot): Boolean = prevMinMaxKeyOpt match {
-      case None => true
-      case Some(prevMinMaxKey) => prevMinMaxKey._2 < currMinMaxKey._1
-    }
-
-    def validationWorkerOrderingAux(workerToMinMaxKeySortedListAux: List[(Worker, Pivot)], workersAux: List[Worker], prevMinMaxKeyOpt: Option[Pivot]): Boolean = {
-      (workerToMinMaxKeySortedListAux, workersAux) match {
-        case (Nil, Nil) => true
-        case (_, Nil) => false
-        case (Nil, _) => false
-        case _ => {
-          val (currWorker, currMinMaxKey) = workerToMinMaxKeySortedListAux.head
-          if (validateMinMaxKeyDependency(prevMinMaxKeyOpt, currMinMaxKey) && (currWorker == workersAux.head) && (workersAux.head._2._1 <= currMinMaxKey._1 && currMinMaxKey._2 <= workersAux.head._2._2)) {
-            validationWorkerOrderingAux(workerToMinMaxKeySortedListAux.tail, workersAux.tail, Some(currMinMaxKey))
-          }
-          else {
-            false
-          }
-        }
-      }
-    }
-
-    val workerToMinMaxKeyList = workerToMinMaxKey.toList
-    val workerToMinMaxKeySortedList = workerToMinMaxKeyList.sortWith((elem0, elem1) => elem0._2._2 < elem1._2._2)
-
-    validationWorkerOrderingAux(workerToMinMaxKeySortedList, workers, None)
-  }
 }
